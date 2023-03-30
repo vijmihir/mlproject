813249cc32f4b5a667209fd47b22a266db01d1e0
Mihir Vij <vijmihiir@gmail.com>
2023-03-30 16:14:27 +0800

    Revised Files

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,7 +1,11 @@
 import os 
 import sys
-from src.exception import CustomException 
-from src.logger import logging 
+import logging 
+sys.path.append('/Users/mihir.vij/Desktop/mlproject')
+print(sys.path)
+
+from src.exception import CustomException
+
 
 import pandas as pd
 from sklearn.model_selection import train_test_split
@@ -14,7 +18,7 @@ class DataIngestionConfig:
     raw_data_path: str = os.path.join('artifacts',"data.csv")
 
 class DataIngestion:
-    def __innit__(self):
+    def __init__(self):
         self.ingestion_config = DataIngestionConfig() # When we call this function the 3 paths will be saved inside this variable
 
     def initiate_data_ingestion(self):
@@ -23,15 +27,17 @@ class DataIngestion:
             df = pd.read_csv('notebook/StudentsPerformance.csv')
             logging.info('Read the dataset as DataFrame')
 
-            os.makedirs(o.path.dirname(self.ingestion_config.train_data_path), exist_ok = True)
+            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path), exist_ok = True)
             df.to_csv(self.ingestion_config.raw_data_path, index = False, header = True)
 
             logging.info('Train Test Split initiated')
             train_set, test_set = train_test_split(df,test_size=0.2, random_state=42)
-            train_set.to_csv(self.ingestion_config.train_set, index = False, header = True)
-            test_set.to_csv(self.ingestion_config.test_set, index = False, header = True)
+
+            train_set.to_csv(self.ingestion_config.train_data_path, index = False, header = True)
+            test_set.to_csv(self.ingestion_config.test_data_path, index = False, header = True)
 
             logging.info('Ingestion of data is complete')
+           
             return (
                 self.ingestion_config.train_data_path, 
                 self.ingestion_config.test_data_path,
@@ -40,6 +46,6 @@ class DataIngestion:
         except Exception as e:
             raise CustomException(e,sys)
 
-if __name__ == "__main__":
-    obj = DataIngestion()
+if __name__=="__main__":
+    obj=DataIngestion()
     obj.initiate_data_ingestion()
