984274b6573d2d0545116c2a3f141c99ca23aa53
Mihir Vij <vijmihiir@gmail.com>
2023-03-31 17:02:49 +0800

    Model Training Performed

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -9,6 +9,8 @@ from src.exception import CustomException
 sys.path.append('/Users/mihir.vij/Desktop/mlproject')
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig 
+from src.components.model_trainer import ModelTrainer
+from src.components.model_trainer import ModelTrainerConfig
 
 
 import pandas as pd
@@ -53,5 +55,9 @@ class DataIngestion:
 if __name__=="__main__":
     obj=DataIngestion()
     train_data, test_data = obj.initiate_data_ingestion()
+
     data_transformation = DataTransformation()
-    data_transformation.initiate_data_transformation(train_data, test_data)
+    train_arr, test_arr,_= data_transformation.initiate_data_transformation(train_data, test_data)
+
+    modeltrainer = ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr, test_arr))
